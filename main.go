fa438fb7b25e06a3ab0a51ed1a29f80e4ee0d18f
Aelpxy <aelpxy@protonmail.com>
2022-10-11 10:54:43 +0100

    feat: Use constant variables to declare image names

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	util "github.com/aelpxy/dbctl/utils"
 )
 
+const POSTGRES_DEFAULT_IMAGE string = "postgres:14.5-alpine"
+
 func main() {
 	containerName := flag.String("name", "postgres-db", "Name to use for the database container.")
 	dbType := flag.String("db", "postgres", "Type of database to deploy.")
@@ -14,7 +16,7 @@ func main() {
 
 	if *dbType == "postgres" {
 		fmt.Printf("Port: %s | Type: %s, Name: %s \n", *dbPort, *dbType, *containerName)
-		util.Pull_Image("postgres:14.5-alpine")
+		util.Pull_Image(POSTGRES_DEFAULT_IMAGE)
 	}
 
 	flag.Parse()
