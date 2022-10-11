8f09e2fc6c1428fb61ac708fa4147dd700049219
Aelpxy <aelpxy@protonmail.com>
2022-10-11 10:52:06 +0100

    feat: Added docker pull function to docker.go

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
+
+	util "github.com/aelpxy/dbctl/utils"
 )
 
 func main() {
-	log.Println("dbctl")
+	containerName := flag.String("name", "postgres-db", "Name to use for the database container.")
+	dbType := flag.String("db", "postgres", "Type of database to deploy.")
+	dbPort := flag.String("port", "5432", "The port on which the database will listen.")
+
+	if *dbType == "postgres" {
+		fmt.Printf("Port: %s | Type: %s, Name: %s \n", *dbPort, *dbType, *containerName)
+		util.Pull_Image("postgres:14.5-alpine")
+	}
+
+	flag.Parse()
 }
---
+++ utils/docker.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func Pull_Image(imageName string) {
+	cmd := exec.Command("docker", "pull", imageName)
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd.Start()
+
+	buf := bufio.NewReader(stdout)
+	for {
+		line, _, _ := buf.ReadLine()
+		if line == nil {
+			log.Println("Successfully pulled docker image.")
+			os.Exit(0)
+		}
+		log.Println(string(line))
+	}
+}
+
+func Create_Database(name, dir string, port int) {
+
+}
