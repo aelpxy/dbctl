d78df701193bb63c3c41b51f80f9d6dc7e557571
Aelpxy <aelpxy@protonmail.com>
2024-05-31 14:05:49 +0600

    refactor: delete_container
    adds:
    -spinner for intuitive ui
    
    fixes:
    - stopping containers properly
    - removing volumes if v is set to true
    - removes the `for` loop that was looping through `volumes.Volumes`
    - no filters

--- docker/delete_container.go
+++ docker/delete_container.go
@@ -7,18 +7,17 @@ import (
 	"time"
 
 	"github.com/aelpxy/dbctl/config"
+	"github.com/briandowns/spinner"
 	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/filters"
-	"github.com/docker/docker/api/types/volume"
 )
 
-func DeleteContainer(containerID string, deleteVolume bool) error {
+func DeleteContainer(containerId string, deleteVolume bool) error {
 	dockerClient, err := DockerClient()
 	if err != nil {
 		return fmt.Errorf("error creating docker client: %w", err)
 	}
 
-	Tcontainer, err := dockerClient.ContainerInspect(context.Background(), containerID)
+	Tcontainer, err := dockerClient.ContainerInspect(context.Background(), containerId)
 	if err != nil {
 		return fmt.Errorf("error inspecting container: %w", err)
 	}
@@ -27,16 +26,24 @@ func DeleteContainer(containerID string, deleteVolume bool) error {
 		return fmt.Errorf("this container %s is not managed by dbctl", Tcontainer.Name)
 	}
 
-	err = dockerClient.ContainerStop(Ctx, containerID, container.StopOptions{})
+	containerSpinner := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
+	containerSpinner.Suffix = fmt.Sprintf(" Stopping container %s... \n", strings.TrimPrefix(Tcontainer.Name, "/"))
+	containerSpinner.Color("green")
+	containerSpinner.Start()
+
+	err = dockerClient.ContainerStop(context.Background(), Tcontainer.ID, container.StopOptions{})
 
 	if err != nil {
 		return fmt.Errorf("error stopping container: %w", err)
 	}
 
-	err = dockerClient.ContainerRemove(Ctx, containerID, container.RemoveOptions{
+	err = dockerClient.ContainerRemove(context.Background(), Tcontainer.ID, container.RemoveOptions{
 		RemoveVolumes: deleteVolume,
 		Force:         true,
 	})
+
+	containerSpinner.Stop()
+
 	if err != nil {
 		return fmt.Errorf("error removing container: %w", err)
 	}
@@ -44,26 +51,29 @@ func DeleteContainer(containerID string, deleteVolume bool) error {
 	if deleteVolume {
 		// this waits for the container to be fully removed before deleting the volume
 		for {
-			_, err := dockerClient.ContainerInspect(Ctx, containerID)
+			_, err := dockerClient.ContainerInspect(context.Background(), Tcontainer.ID)
 			if err != nil {
 				break
 			}
 			time.Sleep(1 * time.Second)
 		}
 
-		filter := filters.NewArgs()
-		filter.Add("name", config.DockerVolumeName+containerID)
-		volumes, err := dockerClient.VolumeList(Ctx, volume.ListOptions{Filters: filter})
+		// faily hacky solution
+		volumeName := config.DockerVolumeName + Tcontainer.Name[len(fmt.Sprintf("/%s", config.DockerContainerPrefix)):]
+
+		fmt.Printf("Deleted volume %s\n", volumeName)
+
+		volumeSpinner := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
+		volumeSpinner.Suffix = fmt.Sprintf(" Deleting volume %s... \n", volumeName)
+		volumeSpinner.Color("green")
+		volumeSpinner.Start()
+
+		err = dockerClient.VolumeRemove(context.Background(), volumeName, true)
+
+		volumeSpinner.Stop()
 
 		if err != nil {
-			return fmt.Errorf("error listing volumes: %w", err)
-		}
-
-		for _, volume := range volumes.Volumes {
-			err = dockerClient.VolumeRemove(Ctx, volume.Name, true)
-			if err != nil {
-				return fmt.Errorf("error removing volume: %w", err)
-			}
+			return fmt.Errorf("error removing volume: %w", err)
 		}
 	}
 
