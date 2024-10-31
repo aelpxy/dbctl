1a0a2f26521ed6f55c62990b230cf6984481699c
aelpxy <aelpxy@protonmail.com>
2024-10-31 18:18:37 +0600

    fix: bug with delete flag

--- cmd/delete_cmd.go
+++ cmd/delete_cmd.go
@@ -8,19 +8,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
+	deleteCmd.Flags().BoolP("force", "f", true, "Delete the associated volume")
+
+	rootCmd.AddCommand(deleteCmd)
+}
+
 var deleteCmd = &cobra.Command{
 	Use:     "delete <container-id>",
 	Short:   "Stop and delete a database",
 	Long:    "This command stops and removes a database container",
 	Aliases: []string{"rm"},
-	Example: `dbctl delete container-id
-dbctl delete container-id --v true
-	`,
-	Args: cobra.MinimumNArgs(1),
+	Args:    cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		containerId := args[0]
 
-		deleteVolume, err := cmd.Flags().GetBool("f")
+		deleteVolume, err := cmd.Flags().GetBool("force")
 
 		if err != nil {
 			log.Fatalf("Error getting volume flag: %v", err)
@@ -33,11 +36,6 @@ dbctl delete container-id --v true
 		}
 
 		fmt.Printf("Container %s has been deleted.\n", containerId)
+
 	},
-}
-
-func init() {
-	deleteCmd.Flags().BoolP("force", "f", true, "Delete the associated volume")
-
-	rootCmd.AddCommand(deleteCmd)
 }
